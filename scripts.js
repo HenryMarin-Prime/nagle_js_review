e05f629b01d49011a94a952116d7a90b089e04dc
HenryMarin-Prime <henrymarin@rocketmail.com>
2020-05-11 15:49:35 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,55 @@
+console.log('hello');
+
+let thisThing = '';
+
+let thisThing2;
+
+let thisNumber = 0;
+
+console.log("1" + "12");
+
+console.log('12' - '1');
+
+console.log('12' * '2');
+
+console.log('12' / '2');
+
+console.log('twelve' / '2');
+
+
+function addinator(numberOne, numberTwo){
+    console.log(numberOne, numberTwo);
+    return numberOne / numberTwo
+}
+
+addinator('twelve', '2');
+
+null
+undefined
+
+//not defined
+
+//if else
+
+if (value1 > value2){
+    //do this
+}
+else if (value1 === value2){
+    //do this
+}
+else{
+//do this
+}
+
+//**Functions**
+
+function doubleIt(x){
+  return  x * 2;
+};
+
+function halfIt(x){
+    x / 2;
+};
+
+console.log(doubleIt(3));//returns 6 on console
+console.log(halfIt(12));//returns undefined on console
